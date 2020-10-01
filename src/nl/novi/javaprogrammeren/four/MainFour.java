f2c8aee424f8e3f927fbeaa62cfdab5a5356c94f
OzgurAbaci <o.abaci@novi-education.nl>
2020-10-01 16:29:01 +0200

    4 af

--- src/nl/novi/javaprogrammeren/four/MainFour.java
+++ src/nl/novi/javaprogrammeren/four/MainFour.java
@@ -1,7 +1,13 @@
 package nl.novi.javaprogrammeren.four;
 
+import nl.novi.javaprogrammeren.four.hidden.PrisonCell;
+
 public class MainFour {
     public static void main(String[] args) {
-
+        PrisonCell pc = new PrisonCell();
+        pc.setInhabitant("Pietje");
+        System.out.println(pc.getInhabitantName());
+        pc.setInhabitantName("Klaasje");
+        System.out.println(pc.getInhabitantName());
     }
 }
--- src/nl/novi/javaprogrammeren/four/hidden/PrisonCell.java
+++ src/nl/novi/javaprogrammeren/four/hidden/PrisonCell.java
@@ -1,4 +1,17 @@
 package nl.novi.javaprogrammeren.four.hidden;
 
 public class PrisonCell {
+    Inhabitant inhabitant;
+
+    public void setInhabitant(String name){
+        this.inhabitant = new Inhabitant(name);
+    }
+
+    public String getInhabitantName(){
+        return this.inhabitant.getName();
+    }
+
+    public void setInhabitantName(String name){
+        inhabitant.setName(name);
+    }
 }
